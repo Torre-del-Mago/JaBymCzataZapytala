40945cf2567f1aaaa3f52b7ae3c3e1d116866268
Bezimienii <czerminskijakub@gmail.com>
2024-05-09 21:37:25 +0200

    Started implementing repositories,

---
+++ Hotel/Hotel/Query/Repository/HotelInfoRepository/HotelInfoRepository.cs
@@ -0,0 +1,63 @@
+﻿using Messages;
+using MongoDB.Driver;
+using Hotel.Query.Model;
+using Hotel.Query.Repository.ReservationRepository;
+using Microsoft.VisualBasic;
+
+namespace Hotel.Query.Repository.HotelInfoRepository
+{
+    public class HotelInfoRepository: IHotelInfoRepository
+    {
+        const string connectionUri = "mongodb://mongo:27017";
+        private IReservationRepository _reservationRepository { get; set; }
+        public HotelInfoRepository(IReservationRepository repository) {
+            _reservationRepository = repository;
+        }
+
+
+        public async Task<HotelQueryResponse> getTripInfo(HotelQuery query)
+        {
+            /*
+             getBasicHotelInfo();
+             getNonbasicHotelInfo();
+             */
+            var reservationTask = _reservationRepository.GetReservationsByHotelIdAndDate(query.HotelId, query.From, query.To);
+
+            var client = new MongoClient(connectionUri);
+            var database = client.GetDatabase("hotel_read");
+            var hotelCollection = database.GetCollection<Model.Hotel>("hotels");
+            var filter = Builders<Model.Hotel>.Filter.Eq<int>(h => h.Id, query.HotelId);
+            var hotel = hotelCollection.Find(filter).FirstOrDefault();
+
+            if (hotel == null)
+            {
+                return null;
+            }
+
+            var response = new HotelQueryResponse
+            {
+                City = hotel.City,
+                Country = hotel.Country,
+                Discount = hotel.Discount,
+                FromDate = query.From,
+                ToDate = query.To
+            };
+
+            List<Messages.Diet> diets = hotel.Diets.Select(d => new Messages.Diet { Id = d.DietId, Name = d.Name }).ToList();
+            response.Diets = diets;
+
+            List<Reservation> reservations = await reservationTask;
+
+            
+        }
+
+        /*
+         
+         */
+
+        public async Task<HotelListQueryResponse> getTripListInfo(HotelListQuery query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hotel/Hotel/Query/Repository/ReservationRepository/ReservationRepository.cs
@@ -0,0 +1,38 @@
+﻿using Hotel.Query.Model;
+using MongoDB.Driver;
+
+namespace Hotel.Query.Repository.ReservationRepository
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        const string connectionUri = "mongodb://mongo:27017";
+
+        MongoClient _client {get; set;}
+        IMongoDatabase _database { get; set;}
+        IMongoCollection<Reservation> _reservationCollection { get; set;}
+
+        public ReservationRepository() { 
+            _client = new MongoClient(connectionUri);
+            _database = _client.GetDatabase("hotel_read");
+        }
+        public async Task addReservation(Reservation reservation)
+        {
+            _reservationCollection.InsertOne(reservation);
+            return;
+        }
+
+        public async Task deleteReservation(int reservationId)
+        {
+            var filter = Builders<Reservation>.Filter.Eq<int>(r => r.Id, reservationId);
+            _reservationCollection.DeleteOne(filter);
+            return;
+        }
+
+        public async Task<List<Reservation>> GetReservationsByHotelIdAndDate(int HotelId, DateOnly fromDate, DateOnly toDate)
+        {
+            var builder = Builders<Reservation>.Filter;
+            var filter = builder.Eq<int>(r => r.Id, HotelId) & builder.Lt<DateOnly>(r => r.FromDate, fromDate) & builder.Gt<DateOnly>(r => r.ToDate, toDate);
+            return _reservationCollection.Find(filter).ToList();
+        }
+    }
+}
